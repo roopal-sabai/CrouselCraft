39e67c2d459e6cf10c849f051e52926753ac5967
roopal-sabai <roopal.sabai@gmail.com>
2026-06-18 14:28:55 +0530

    Auto-redirect to /auth if shop query param is provided to bypass recurring login screen

--- app/routes/auth.login/route.jsx
+++ app/routes/auth.login/route.jsx
@@ -1,10 +1,16 @@
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { useState } from "react";
-import { Form, useActionData, useLoaderData } from "react-router";
+import { Form, useActionData, useLoaderData, redirect } from "react-router";
 import { login } from "../../shopify.server";
 import { loginErrorMessage } from "./error.server";
 
 export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const shop = url.searchParams.get("shop");
+  if (shop) {
+    throw redirect(`/auth?${url.searchParams.toString()}`);
+  }
+
   const errors = loginErrorMessage(await login(request));
 
   return { errors };
